6cd7c98f7bfe87eeb9635965ad95b3b31da43dc0
lawheitung <45755541+lawheitung@users.noreply.github.com>
2021-11-09 22:51:33 +0800

    bug fix

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import JourneyList from '../components/journey/journey';
-import QualityList from '../components/gameplan/gameplan';
-import FencingList from '../components/fencing/fencing';
-import Hero from '../components/hero/hero'
+import React from 'react'
+import JourneyList from '../components/journey/journey'
+import QualityList from '../components/gameplan/gameplan'
+import FencingList from '../components/fencing/fencing'
 import Contact from '../components/contact'
 import TravelList from '../components/travel/travel'
 
